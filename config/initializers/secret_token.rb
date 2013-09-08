5eccd2bcb2dae7677dd62da62bb95bae381a6454
Abhishek Mukhopadhyay <amukhop1@server.fake>
2013-09-08 18:19:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMoneyManager3::Application.config.secret_token = '95bd44f106b14484b5d3c34562cad62ccaf19b164f75063a1931d5d35b2dc2d9a8d82dde32eedc67c50eef92f9afdb04cf63ab49410bc377265119f73a9f0c54'
